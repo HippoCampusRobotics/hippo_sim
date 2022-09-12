45bdfd7e799aca606d7cbf2f67085bea1ee1dd76
Thies Lennart Alff <lennart.alff@gmail.com>
2022-09-12 13:19:41 +0200

    renamed hippo2 to hippo3

--- launch/spawn_vehicle.launch.py
+++ launch/spawn_vehicle.launch.py
@@ -13,7 +13,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     package_path = get_package_share_path('hippo_sim')
     world = package_path / 'world' / 'empty.sdf'
-    default_model_path = package_path / 'urdf/hippo2.xacro'
+    default_model_path = package_path / 'urdf/hippo3.xacro'
     default_vehicle_name = 'uuv00'
 
     model_launch_arg = DeclareLaunchArgument(
